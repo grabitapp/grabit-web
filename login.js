e62b7db930f8666918a9d3f2dc748ced4e76ba91
Sammi De Guzman <sammi@sammdot.ca>
2016-11-20 08:47:48 -0500

    merge login and signup pages

---
+++ login.js
@@ -0,0 +1,22 @@
+let txtEmail = document.getElementById('txtEmail');
+let txtPassword = document.getElementById('txtPassword');
+let btnLogin = document.getElementById('btnLogin');
+let btnSignupVirtual = document. getElementById('btnSignupVirtual');
+
+btnSignupVirtual.addEventListener("click", e => window.location.href = "/signup");
+
+//add login event
+btnLogin.addEventListener('click', e => {
+ //get username and password
+    const email = txtEmail.value;
+    const password = txtPassword.value;
+    const auth = firebase.auth();
+    
+    //Sign in
+    const promise = auth.signInWithEmailAndPassword(email, password);
+    promise.then(e => {
+        window.location.href = "/tasks";
+    }, e => {
+        window.alert(e.message);
+    });
+});
