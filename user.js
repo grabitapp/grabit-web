47b91c5e4a49e823652ddf24915adf99194ddf4f
vickywang410 <vickywang410@gmail.com>
2016-11-20 06:58:10 -0500

    Login and Signup Backends with Button Errors

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-//(function(){
+//function go(){
     
     const config = {
         apiKey: "AIzaSyCskIAWEozzbq2qUm3YXe3i2cJp0TBi_Ag",
@@ -9,12 +9,12 @@
       };
     
     firebase.initializeApp(config);
-    console.log('am i running')
+    console.log('am i running');
     //get elements
     let txtEmail = document.getElementById('txtEmail');
     let txtPassword = document.getElementById('txtPassword');
     let btnLogin = document.getElementById('btnLogin');
-//    let btnSignup = document.getElementById('btnSignup');
+//  let btnSignup = document.getElementById('btnSignup');
     
     console.log(btnLogin);
 
@@ -26,11 +26,40 @@
         console.log(email)
         console.log(password)
         const auth = firebase.auth();
-
+        
         //Sign in
         const promise = auth.signInWithEmailAndPassword(email, password);
-        promise.catch(e => console.log(e.message));
-    });
+        promise.catch(e => {
+            console.log(e.message);
+            window.location.href = "tasks.html";
+        });
+
+  //    auth.onAuthStateChanged(user => {
+  //  if const auth = firebase.auth()
+  //  {
+                // Signed in
+  //             window.location.href = "tasks.html";
+   //        }
+   // else { alert('user ' + ' does not exist!');}
+//    });
+        
+//function go() {
+  //var userId = prompt('email', 'Guest');
+  //checkIfUserExists(userId);
+//}
+       
+//var USERS_LOCATION = 'https://console.firebase.google.com/project/grabit-e26a4/authentication/users';
+//function userExistsCallback(userId, exists) {
+  //if (exists) {
+//   //switch to task.html
+  //    window.location.href = "task.html"
+  //} 
+//    else {
+//    alert('user ' + ' does not exist!');
+//    }
+//}
+})
+   
     
    //add signup event
 //    btnSignup.addEventListener('click', e => {
