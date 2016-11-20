add1eb08eb586b3502a18ac5b06316ee7e43db79
vickywang410 <vickywang410@gmail.com>
2016-11-19 23:52:08 -0500

    Merged Signin Page

--- user.js
+++ user.js
@@ -1,46 +1,55 @@
-(function(){
+//(function(){
     
     const config = {
-    apiKey: "AIzaSyBOB37GxfcCQYeey8WlHu9wfAFuAp0VFiQ",
-    authDomain: "login-d72aa.firebaseapp.com",
-    databaseURL: "https://login-d72aa.firebaseio.com",
-    storageBucket: "",
-    messagingSenderId: "80928384473"
-};
+        apiKey: "AIzaSyCskIAWEozzbq2qUm3YXe3i2cJp0TBi_Ag",
+        authDomain: "grabit-e26a4.firebaseapp.com",
+        databaseURL: "https://grabit-e26a4.firebaseio.com",
+        storageBucket: "grabit-e26a4.appspot.com",
+        messagingSenderId: "548667916114"
+      };
+    
     firebase.initializeApp(config);
-    
+    console.log('am i running')
     //get elements
-    const txtUserName = document.getElementById('txtUserName');
-    const txtPassword = document.getElementById('txtPassword');
-    const btnLogin = document.getElementById('btnLogin');
-    const btnSignup = document.getElementById('btnSignup');
+    let txtUserName = document.getElementById('txtUserName');
+    let txtPassword = document.getElementById('txtPassword');
+    let btnLogin = document.getElementById('btnLogin');
+//    let btnSignup = document.getElementById('btnSignup');
     
+    console.log(btnLogin);
+
     //add login event
     btnLogin.addEventListener('click', e => {
      //get username and password
         const username = txtUserName.value;
         const password = txtPassword.value;
+        console.log(username)
+        console.log(password)
+        const auth = firebase.auth();
+
         //Sign in
-        const promise = auth.signInWithUsernameAndPassword(username, pass);
+        const promise = auth.signInWithEmailAndPassword(username, password);
         promise.catch(e => console.log(e.message));
     });
     
    //add signup event
-    btnSignup.addEventListener('click',e =>{
-         const username = txtUserName.value;
-        const password = txtPassword.value;
-        //Create user
-        const promise = auth.createUserWithUsernameAndPassword(username, pass);
-        promise
-            .catch(e => console.log(e.message));
-        
-    });
+//    btnSignup.addEventListener('click', e => {
+//        const username = txtUserName.value;
+//        const password = txtPassword.value;
+//        //Create user
+//        const auth = firebase.auth();
+//        const promise = auth.createUserWithEmailAndPassword(username, password);
+//        promise.catch(e => console.log(e.message));
+//        
+//    });
     //add a realtime listener
-    firebase.auth().onAuthStateChanged(firebaseUser => {
-        if(firebaseUser){
-            console.log(firebaseUser);
-        }
-        else{
-            console.log('Not logged in');
-        }
-    }); 
+//    firebase.auth().onAuthStateChanged(firebaseUser => {
+//        if(firebaseUser){
+//            console.log(firebaseUser);
+//        }
+//        else{
+//            console.log('Not logged in');
+//        }
+//    });
+    
+//})
