fb86819fa5b6e5706fde5fa39ff4104f0776432c
Sammi De Guzman <sammi@sammdot.ca>
2016-11-19 20:18:13 -0500

    move firebase initialization to its own script

---
+++ firebase.js
@@ -0,0 +1,7 @@
+firebase.initializeApp({
+	apiKey: "AIzaSyCskIAWEozzbq2qUm3YXe3i2cJp0TBi_Ag",
+	authDomain: "grabit-e26a4.firebaseapp.com",
+	databaseURL: "https://grabit-e26a4.firebaseio.com",
+	storageBucket: "",
+	messagingSenderId: "548667916114"
+});
